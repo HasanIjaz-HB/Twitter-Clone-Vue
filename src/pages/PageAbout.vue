910fb3985841198e462dd317e2d7c693d932b697
HasanIjaz-HB <hasanijaz@sabanciuniv.edu>
2021-04-05 01:10:47 +0500

    update pageabout

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,7 +1,11 @@
 <template>
-  <q-page>
-    <h5>About</h5>
-  </q-page>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Vuetter</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashd</p>
+      <p>Lorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashdLorem ipsum dolor sadklasjduoqwbdojasldks/adasdasdmaskdbashd</p>
+    </div>
+      </q-page>
 </template>
 
 <script>
