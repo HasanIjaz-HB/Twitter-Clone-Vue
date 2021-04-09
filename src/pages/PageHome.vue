b9ea93059725a1f67e675935f97298b24cf62526
HasanIjaz-HB <hasanijaz@sabanciuniv.edu>
2021-04-10 01:32:32 +0500

    add new vuetss to firebase and display

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -129,7 +129,14 @@ export default {
         content: this.newVuettContent,
         date: Date.now()
       };
-      this.vuetts.unshift(newVuett);
+      db.collection("vuetts")
+        .add(newVuett)
+        .then(docRef => {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(error => {
+          console.error("Error adding document: ", error);
+        });
       this.newVuettContent = "";
     },
     deleteVuett(vuett) {
